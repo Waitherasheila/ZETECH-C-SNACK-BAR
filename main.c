74129dea116675fa91139414ac77f6d04c524697
Sheila Waithera <106369293+Waitherasheila@users.noreply.github.com>
2022-07-14 15:27:05 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int snack;
+    printf("ZETECH C SNACK BAR\n");
+    printf("1. SMOKIE - Kshs. 40\n");
+    printf("2. MILKSHAKE - Kshs. 150\n");
+    printf("3. HAMBURGER - Kshs. 300\n");
+    printf("4. SAMOSA - Kshs. 50\n");
+    printf("5. AFRICAN TEA - Kshs. 60\n");
+    return 0;
+}
